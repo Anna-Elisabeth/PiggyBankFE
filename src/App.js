21f8a516a0c3477213bb4e00929ccdf78c001e5e
Anna Wilson <anna.elisabeth@gmail.com>
2024-05-07 12:19:43 +0100

    home and nav

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/home/Home';
+import piggy1 from "./piggy1.png";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <body>
+     
+
+
+      <div>
+    <BrowserRouter>
+    
+    {/* <!-- Navbar --> */}
+<nav class="navbar navbar-expand-lg navbar-dark bg-light gradient-custom">
+    {/* <!-- Container wrapper --> */}
+    <div class="container-fluid">
+        {/* <!-- Toggle button --> */}
+        <button
+            class="navbar-toggler text-white"
+            type="button"
+            data-mdb-toggle="collapse"
+            data-mdb-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
         >
-          Learn React
-        </a>
-      </header>
+            <i class="fas fa-bars"></i>
+        </button>
+
+        {/* <!-- Collapsible wrapper --> */}
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            {/* <!-- Navbar brand --> */}
+            <a class="navbar-brand mt-2 mt-lg-0" href="#">
+    <img src={piggy1} alt="Logo" style={{width: '100%', height: '100%'}} />
+</a>
+
+            {/* <!-- Left links --> */}
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item" style={{ marginLeft: '100px' }}>
+                    <a class="nav-link" href="/">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/handyhints">Handy Hints</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/creditscore">Credit Score</a>
+                </li>
+                
+                
+            </ul>
+            {/* <!-- Left links --> */}
+        </div>
+        {/* <!-- Collapsible wrapper --> */}
+
+      
+           
+
+        {/* <!-- Right elements --> */}
     </div>
+    {/* <!-- Container wrapper --> */}
+</nav>
+{/* <!-- Navbar --> */}
+
+  
+    
+     
+      <Routes>
+      <Route path='/' element={<Home />} />
+     
+      
+     
+      
+        </Routes>
+
+
+
+
+    </BrowserRouter>
+
+
+
+  </div>
+
+</body>
+
+
+
   );
 }
 
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+function Home() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Add your login logic here
+        console.log('Username:', username);
+        console.log('Password:', password);
+    };
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={handleUsernameChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                    />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
+
+export default Home;
